b398e52aa71d3f6e9091d05aadd64fe1f05da5fd
Greeshma M <greeshmapadinjakkara@gmail.com>
2025-07-06 16:45:39 +0530

    Add files via upload

---
+++ Setup_Code.ino
@@ -0,0 +1,10 @@
+const int MQ135_Pin = A0;
+void setup() {
+Serial.begin(9600);
+}
+void loop() {
+int sensorValue = analogRead(MQ135_Pin);
+Serial.print("MQ-135 Value: ");
+Serial.println(sensorValue);
+delay(1000);
+}
